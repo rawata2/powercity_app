230ce670c912c21412d8f3cbed8107fdeb344d13
rawata2 <arvind.rawat2@mail.dcu.ie>
2022-12-07 16:58:24 +0000

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import { NavBar } from './ui-components';
 
 function App() {
   return (
-    <div>
-      <h1> Hello World! </h1>
-    </div>
+    <NavBar />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ThemeProvider } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+
+import awsconfig from './aws-exports';
+
+import "@aws-amplify/ui-react/styles.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={studioTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
