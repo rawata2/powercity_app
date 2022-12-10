4ef801c9b0a075f5b36dda1733f218db76737eb9
IamMatt117 <81575818+IamMatt117@users.noreply.github.com>
2022-12-10 00:19:47 +0000

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { TallCardCollection, NavBar } from './ui-components';
+import { TallCardCollection, NavBar, MarketingFooter } from './ui-components';
 
 function App() {
   return (
     <div className='App'>
       <NavBar width={"100vw"}/>
       <TallCardCollection/>
+      <div id="footer"><MarketingFooter/></div>
     </div>
   );
 }
